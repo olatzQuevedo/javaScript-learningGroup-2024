625ba09ec8a4ebd261a6f950a0729c7d393963d0
olatzQuevedo <olatzquevedotorres@gmail.com>
2023-12-28 19:48:16 +0100

    añadir el ejercicio de indexOf

---
+++ staff/olatz-quevedo/strings/indexOf.js
@@ -0,0 +1,13 @@
+//DECLARAMOS LA FUNCIÓN Y LE AÑADIMOS LOS PARÁMETROS
+function indexOfFunction(text, charToSearch) {
+//CREAMOS EL BUCLE EN EL QUE LE DAMOS UNA CONDICIÓN, LE DECIMOS HASTA DONDE QUEREMOS QUE LO HAGA, Y LO QUE HACE SI NO LLEGA AL FINAL
+    for (i = 0; i < text.length; i++) {
+//PONEMOS UN CONDICIONANTE PARA PODER CUMPLIR LO QUE QUEREMOS EN EL BUCLE. QUE EN ESTE CASO ES QUE NOS DEVUELVA EL NUMERO DE LA POSICIÓN DE LA LETRA QUE ESCOGEMOS
+        if (text[i] === charToSearch) {
+
+            return i
+        }
+    }//Y EN CASO DE QUE LA LETRA NO ESTE NOS DEVUELVA UN -1
+    return -1
+}
+console.log(indexOfFunction('Kratos', '0'))
