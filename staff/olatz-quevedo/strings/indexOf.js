66fcf60436cf4079f66dc24dd892d8e89c65226a
olatzQuevedo <olatzquevedotorres@gmail.com>
2023-12-28 20:05:40 +0100

    modificación del ejercicio añadiendo startingIndex

--- staff/olatz-quevedo/strings/indexOf.js
+++ staff/olatz-quevedo/strings/indexOf.js
@@ -1,8 +1,10 @@
 //DECLARAMOS LA FUNCIÓN Y LE AÑADIMOS LOS PARÁMETROS
-function indexOfFunction(text, charToSearch) {
-//CREAMOS EL BUCLE EN EL QUE LE DAMOS UNA CONDICIÓN, LE DECIMOS HASTA DONDE QUEREMOS QUE LO HAGA, Y LO QUE HACE SI NO LLEGA AL FINAL
+function indexOfFunction(text, charToSearch, startingIndex) {
+    let start = 0
+    if (startingIndex > 0) start = startingIndex
+    //CREAMOS EL BUCLE EN EL QUE LE DAMOS UNA CONDICIÓN, LE DECIMOS HASTA DONDE QUEREMOS QUE LO HAGA, Y LO QUE HACE SI NO LLEGA AL FINAL
     for (i = 0; i < text.length; i++) {
-//PONEMOS UN CONDICIONANTE PARA PODER CUMPLIR LO QUE QUEREMOS EN EL BUCLE. QUE EN ESTE CASO ES QUE NOS DEVUELVA EL NUMERO DE LA POSICIÓN DE LA LETRA QUE ESCOGEMOS
+        //PONEMOS UN CONDICIONANTE PARA PODER CUMPLIR LO QUE QUEREMOS EN EL BUCLE. QUE EN ESTE CASO ES QUE NOS DEVUELVA EL NUMERO DE LA POSICIÓN DE LA LETRA QUE ESCOGEMOS
         if (text[i] === charToSearch) {
 
             return i
@@ -10,4 +12,4 @@ function indexOfFunction(text, charToSearch) {
     }//Y EN CASO DE QUE LA LETRA NO ESTE NOS DEVUELVA UN -1
     return -1
 }
-console.log(indexOfFunction('Kratos', '0'))
+console.log(indexOfFunction('Kratos', 'a'))
