394b337aac20d2274c84dc8c173388a597e26ba2
olatzQuevedo <olatzquevedotorres@gmail.com>
2024-01-03 11:49:43 +0100

    añadir el ejercicio de some

---
+++ staff/olatz-quevedo/arrays/some.js
@@ -0,0 +1,31 @@
+const numbers = [45, 65, 18, 76, 98, 34]
+
+function someFunction(array, callbackFunction) {
+
+    let result = false
+    array.forEach(item => {
+
+        const resultCallBack = callbackFunction(item)
+
+        if (resultCallBack) {
+            result = true
+
+        }
+
+    })
+    return result
+}
+
+function checkAge1(age) {
+    return age >= 90
+}
+
+function checkAge2(age) {
+    return age < 18
+}
+
+console.log(someFunction(numbers, checkAge1))
+// true
+
+console.log(someFunction(numbers, checkAge2))
+// false
