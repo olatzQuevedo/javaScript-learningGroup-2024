34a5c5c7b69b42f8f101e5205d909791ca20da3d
olatzQuevedo <olatzquevedotorres@gmail.com>
2024-01-04 20:06:15 +0100

    añadir el ejercicio de findIndex

--- staff/olatz-quevedo/arrays/findIndex.js
+++ staff/olatz-quevedo/arrays/findIndex.js
@@ -2,10 +2,25 @@ const numbers = [5, 10, 8, 20, 70, 60]
 
 function findFunction(array, callbackFunction) {
 	// TODO: IMPLEMENTAR EL CÓDIGO
+
+	let result = -1
+	array.forEach((item, index) => {
+
+		const resultCallBack = callbackFunction(item)
+
+		/* if(result > -1){
+			return
+		} */
+
+		if (resultCallBack && (result === -1)) {
+			result = index
+		}
+	})
+	return result
 }
 
 
-const numberBiggerThan50Function = function(number) {
+const numberBiggerThan50Function = function (number) {
 	return number > 50
 }
 
